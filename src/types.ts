8c7f90e9ebbe5942f303060bee55ccaf900d273f
outlierSlug <143982938+outlierSlug@users.noreply.github.com>
2026-03-24 00:13:18 -0700

    Add comments for types

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,15 @@
+// The two players in the game.
 export type Player = "X" | "O";
-export type Winner = Player | "Draw" | null;
+
+// The value of a single square (cell) on the board.
+// Can be owned by either player or null if not yet selected.
 export type Cell = Player | null;
+
+// The game board represented as a flat array of cells.
 export type GameBoard = Cell[];
+
+// The "winner" of the game. Can be either player, a draw, or null if the game is still ongoing.
+export type Winner = Player | "Draw" | null;
+
+// The result of the game. Contains a record of the winner and the winning squares, if any.
 export type GameResult = { winner: Winner, winningSquares: number[]} ;
