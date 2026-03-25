7eac316feecd9a74d91e828313572f666fece8bc
outlierSlug <143982938+outlierSlug@users.noreply.github.com>
2026-03-24 17:19:13 -0700

    Add CORS to server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,13 @@
 import express, { Express } from "express";
+import cors from "cors";
 import { getGameState, setGameState } from "./routes";
 
 const port: number = 8080;
 const app: Express = express();
 
+app.use(cors());
 app.use(express.json());
 app.get("/game", getGameState);
-app.post("/game", setGameState)
+app.post("/game", setGameState);
 
 app.listen(port, () => console.log(`Server listening on ${port}`));
