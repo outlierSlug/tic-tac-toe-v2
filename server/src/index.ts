7a84ddb3649dd233ad8ceb05caedff5c1d77112f
outlierSlug <143982938+outlierSlug@users.noreply.github.com>
2026-03-28 16:02:49 -0700

    Modify CORS with allowed origins

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,7 +5,13 @@ import { getGameState, setGameState, getSettings, setSettings } from "./routes";
 const port: number = 8080;
 const app: Express = express();
 
-app.use(cors());
+const ALLOWED_ORIGINS: string[] = [
+  "http://localhost:5173", 
+  "https://tic-tac-toe-v2-pink.vercel.app/",
+  "https://tic-tac-toe-v2-outlierslugs-projects.vercel.app"
+];
+
+app.use(cors({ origin: ALLOWED_ORIGINS }));
 app.use(express.json());
 
 app.get("/game", getGameState);
